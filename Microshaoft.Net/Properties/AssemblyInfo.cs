65d54b3f8ccc3d3f317cd0f3f09a4d3c8a5cab1e
user <user@email.com>
2014-03-29 00:15:29 +0800

    signature

--- Microshaoft.Net/Properties/AssemblyInfo.cs
+++ Microshaoft.Net/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microshaoft.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microshaoft")]
 [assembly: AssemblyProduct("Microshaoft.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 于溪玥")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
