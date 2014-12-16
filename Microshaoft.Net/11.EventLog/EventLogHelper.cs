8274353dfb8977c3638861ea0bf5991a39fef052
user <user@email.com>
2014-12-16 10:56:08 +0800

    2014-12-16 EasyReaderWriterLock

--- Microshaoft.Net/11.EventLog/EventLogHelper.cs
+++ Microshaoft.Net/11.EventLog/EventLogHelper.cs
@@ -5,7 +5,7 @@
     using System.Linq;
     using System.Collections.Generic;
     using Microshaoft;
-    public class Program
+    public class Program1
     {
         static void Main(string[] args)
         {
